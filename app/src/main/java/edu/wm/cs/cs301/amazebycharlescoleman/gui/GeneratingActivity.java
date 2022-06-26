6609ab3b29832b86f8274c44315c3d307914ff4b
colemancw <85973709+colemancw@users.noreply.github.com>
2022-06-26 15:28:07 -0400

    first commit

--- app/src/main/java/edu/wm/cs/cs301/amazebycharlescoleman/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebycharlescoleman/gui/GeneratingActivity.java
@@ -1,6 +1,11 @@
 package edu.wm.cs.cs301.amazebycharlescoleman.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 import android.content.Intent;
 import android.widget.ProgressBar;
@@ -10,6 +15,7 @@ import android.os.Bundle;
 import edu.wm.cs.cs301.amazebycharlescoleman.R;
 
 public class GeneratingActivity extends AppCompatActivity {
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,12 @@ public class GeneratingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_generating);
         Intent j = new Intent(getApplicationContext(), PlayManuallyActivity.class);
         startActivity(j);
-        }
+        back = (Button) findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(GeneratingActivity.this, AMazeActivity.class);
+                startActivity(i);
+            }
+        });
     }
+}
